7d45188de730d993f4360d62a36224bf0f326068
Timmutegi <mutegi.timothy538@gmail.com>
2022-09-18 08:00:11 -0600

    Added tests

---
+++ test_mars_rover.py
@@ -0,0 +1,22 @@
+from curses.panel import top_panel
+import pytest
+from MarsRover import MarsRover
+
+top_right_coordinates = {
+    "x": 5,
+    "y": 5
+}
+
+mars_rover = MarsRover(top_right_coordinates)
+
+def test_deploy_rover_failed():
+    with pytest.raises(Exception):
+        mars_rover.deploy_rover(-1, -1, "N", "LMLMLMLMM")
+
+def test_deploy_rover_successful():
+    final_position = mars_rover.deploy_rover(1, 2, "N", "LMLMLMLMM")
+    assert final_position == {
+        'x': 1,
+        'y': 3,
+        'orientation': "N"
+    }
